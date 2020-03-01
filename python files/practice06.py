020de6c24f59a28df45549fc8d8b32f8f8df0ce5
newoni <59407783+newoni@users.noreply.github.com>
2020-03-01 21:08:46 +0900

    Update practice06.py

--- python files/practice06.py
+++ python files/practice06.py
@@ -1,5 +1,9 @@
 # <20.2.28> by KH
 
+'''
+39 page
+Implementing perceptron in Keras
+'''
 import numpy as np
 import pandas as pd
 import seaborn as sns
@@ -25,60 +29,6 @@ d2 = pd.DataFrame({'x1':np.random.normal(mu_x1,sigma_x1,1000) + x2_mu_diff,'x2':
 data = pd.concat([d1,d2], ignore_index = True)
 
 ax = sns.scatterplot(x="x1",y='x2',hue = 'type',data=data)
-
-class Perceptron:
-    # Simpole implementation of the perceptron algorithm
-
-    def __init__(self,w0=1,w1=0.1,w2=0.1):
-        # weights
-
-        self.w0 = w0 # bias
-        self.w1 = w1
-        self.w2 = w2
-
-    def step_function(self,z):
-        if z>=0:
-            return 1
-        else:
-            return 0
-
-    def weighed_sum_inputs(self,x1,x2):
-        return sum([1*self.w0, x1*self.w1, x2*self.w2])
-
-    def predict(self, x1, x2):
-        # Uses the step function to determine the output
-
-        z = self.weighed_sum_inputs(x1,x2)
-        return self.step_function(z)
-
-    def predict_boundary(self, x):
-        # used to predict the boundaries of our classifier
-
-        return -(self.w1 * x + self.w0)/self.w2
-
-    def fit(self,X,y, epochs = 1, step = 0.1, verbose = True):
-        # Train the model given the dataset
-
-        errors = []
-
-        for epoch in range(epochs):
-            error = 0
-            for i in range(0,len(X.index)):
-                x1, x2, target = X.values[i][0], X.values[i][1], y.values[i]
-
-                # The update is proportional to the step size and the error
-
-                update = step * (target - self.predict(x1, x2))
-
-                self.w1 += update * x1
-                self.w2 += update * x2
-                self.w0 += update
-                error += int(update != 0.0)
-
-            errors.append(error)
-
-            if verbose:
-                print('Epochs: {} - Error: {} - Errors from all epochs: {}'.format(epoch,error,errors))
 
 # Splitting the dataset in training and test set
 msk = np.random.rand(len(data))< 0.8
