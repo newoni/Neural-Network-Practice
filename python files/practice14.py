fc03715c94f3d4becb46964714a8fc4fa9486e28
newoni <59407783+newoni@users.noreply.github.com>
2020-03-03 15:51:11 +0900

    Update practice14.py

--- python files/practice14.py
+++ python files/practice14.py
@@ -1,1 +1,24 @@
 # <20.3.3> by KH
+
+'''
+page 91
+Machine learning for NLP
+'''
+
+import nltk
+from nltk.stem.porter import *
+
+
+ps = PorterStemmer()
+
+print(ps.stem('run'))
+print(ps.stem('dogs'))
+print(ps.stem('mice'))
+print(ps.stem('were'))
+
+sentence = "hello, my name is KH. Machine learning is funny!"
+words = nltk.word_tokenize(sentence)
+
+print(words)
+
+print([ps.stem(w) for w in words] )
